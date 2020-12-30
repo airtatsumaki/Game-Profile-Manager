ed430c67700098fc21f5f91bbfb772c0e615bc1a
tatsu <naz1mb20@gmail.com>
2020-12-30 19:25:03 +0000

    updated path notations

--- fileCopyTest.py
+++ fileCopyTest.py
@@ -2,12 +2,12 @@ from distutils.dir_util import copy_tree
 import os
 
 # copy subdirectory example
-fromDirectory = "C:\\Users\\user\\AppData\\Local\\SessionGame\\Saved\\SaveGames"
+fromDirectory = "C:/Users/user/AppData/Local/SessionGame/Saved/SaveGames"
 toDirectory = "backupFolder"
 
-fullPath = "resources\\games\\" + toDirectory
+fullPath = "resources/games/" + toDirectory
 
-if not os.path.exists("resources\\games\\" + toDirectory):
-    os.makedirs("resources\\games\\" + toDirectory)
+if not os.path.exists("resources/games/" + toDirectory):
+    os.makedirs("resources/games/" + toDirectory)
 
-copy_tree(fromDirectory, "resources\\games\\" + toDirectory)
+copy_tree(fromDirectory, "resources/games/" + toDirectory)
